85b004df5380f8ba930aafd06af8c7d10dae59c8
netaro-N <takebbb1+github@gmail.com>
2019-03-30 06:35:38 +0000

    per 3 minutes

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 'use strict'
 const Twitter = require('twitter');
+const cron = require('cron').CronJob;
 require('dotenv').config();
 
 const client = new Twitter({
   consumer_key: process.env.TWITTER_API_CONSUMER_KEY,
-  consumer_secret:process.env.TWITTER_API_CONSUMER_SECRET,
+  consumer_secret: process.env.TWITTER_API_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_API_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_API_ACCESS_TOKEN_SECRET
 });
 
-client.get('statuses/home_timeline', {}, function(error, tweets, response) {
-  if(error) console.log(error);
-  console.log(tweets);
+function getHomeTimeLine() {
+  client.get('statuses/home_timeline', {}, function (error, tweets, response) {
+    if (error) console.log(error);
+    console.log(tweets);
+  });
+}
+
+const cronJob = new cron({
+  cronTime: '00 0-59/3 * * * *', // 3分ごとに実行
+  start: true, // newした後即時実行するかどうか
+  onTick: function() {
+    getHomeTimeLine();
+  }
 });
+
+getHomeTimeLine();
