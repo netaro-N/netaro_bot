d25f94d941d3d8df93eb3724812ac2fb4e47eb21
netaro-N <takebbb1+github@gmail.com>
2019-03-30 05:53:29 +0000

    timeLine & dotenv

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 'use strict'
+const Twitter = require('twitter');
+require('dotenv').config();
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_API_CONSUMER_KEY,
+  consumer_secret:process.env.TWITTER_API_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_API_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_API_ACCESS_TOKEN_SECRET
+});
+
+client.get('statuses/home_timeline', {}, function(error, tweets, response) {
+  if(error) console.log(error);
+  console.log(tweets);
+});
